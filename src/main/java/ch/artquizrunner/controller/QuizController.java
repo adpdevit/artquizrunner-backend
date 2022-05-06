b4adcfd302c1a31e20231508a348f27ebfc13037
adpdevit <adpdevit@gmail.com>
2022-05-06 21:55:22 +0200

    integration of open api codegen
    Adapt curent project to open api codegen

--- src/main/java/ch/artquizrunner/controller/QuizController.java
+++ src/main/java/ch/artquizrunner/controller/QuizController.java
@@ -1,15 +1,20 @@
 package ch.artquizrunner.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.artquizrunner.api.QuizApi;
+import ch.artquizrunner.model.GameState;
+
 @RestController
 @RequestMapping("/api/user/quiz")
-public class QuizController {
+public class QuizController implements QuizApi {
 	
 	@GetMapping("/gamestate")
-    public void getInitialGameState() {
+    public ResponseEntity<GameState> getInitialGameState() {
         // TODO : Send an initial gamestate through cookie or http header
+		return null;
     }
 }
